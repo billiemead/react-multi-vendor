ea11ea41e0a4f2b8afeb5413c40fc17c0bdd7bcc
Billie Mead <bmead@mjhlifesciences.com>
2024-05-04 18:33:45 -0400

    install & integrate jwt token package

---
+++ backend/models/adminModel.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require("mongoose");
+
+const adminSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: false,
+    },
+    role: {
+        type: String,
+        default: 'admin',
+    },
+})
+
+module.exports = model('admins', adminSchema);
--- dashboard/src/store/Reducers/authReducer.js
+++ dashboard/src/store/Reducers/authReducer.js
@@ -1,14 +1,15 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import api from "../../api/api";
 
 export const admin_login = createAsyncThunk(
     'auth/admin_login',
-    async (info) => {
+    async(info) => {
         // console.log(info);
         try {
-            const { data } = await api.post('/admin/login', info, { withCredentials: true });
+            const { data } = await api.post('/admin-login', info, { withCredentials: true });
             console.log(data);
         } catch (error) {
-
+            console.log(error.response.data);
         }
     }
 )
